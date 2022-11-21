83537ee0bbbba4b0f9a30729d0286ae91b6d12a7
NAREK HOVHANNISYAN <narek423@gmail.com>
2022-11-22 00:07:44 +0400

    Download buttons

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Sabmition from "../components/sabmition";
 import ProtectionInformation from "../components/protection-information";
 import VictimsGalery from "../components/victims-galery";
 import ProtectionApp from "../components/protaction-app";
+import AppDownload from "../components/app-download";
 
 export default function Home() {
 	return (
@@ -13,8 +14,8 @@ export default function Home() {
 			<ProtectionInformation />
 			<VictimsGalery />
 			<Sabmition />
-			<ProtectionApp /> //Home project@ page anel meke Pricing@ page ane
-			myusner@ komponent
+			<ProtectionApp />
+			<AppDownload />
 		</div>
 	);
 }
