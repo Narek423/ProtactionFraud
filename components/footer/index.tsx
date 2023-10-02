503314b6aebea05847d0d6957ab8261c71b1ce97
Narek Hovhannisyan <narek423@gmail.com>
2023-10-02 18:21:22 +0400

    Project fully finished

--- components/footer/index.tsx
+++ components/footer/index.tsx
@@ -6,8 +6,8 @@ const Footer: FC = () => {
 		<div className={styles.body}>
 			<div>© 2022 First Guardian Ltd. All rights reserved</div>
 			<div className={styles.social_icons_container}>
-				{["/twitter.svg", "/facebook.svg", "/instagram.svg"].map((elem) => (
-					<img src={elem} />
+				{["/twitter.svg", "/facebook.svg", "/instagram.svg"].map((elem, index) => (
+					<img src={elem} key={Math.random() + index}/>
 				))}
 			</div>
 		</div>
